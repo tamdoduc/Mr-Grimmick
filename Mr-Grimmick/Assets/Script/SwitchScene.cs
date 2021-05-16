d7aca140e08e89d54032a55f160f7671dc88b272
Đỗ Đức Tâm <tamdo2334@gmail.com>
2021-05-16 21:12:15 +0700

    FullScene1

--- Mr-Grimmick/Assets/Script/SwitchScene.cs
+++ Mr-Grimmick/Assets/Script/SwitchScene.cs
@@ -6,11 +6,15 @@ public class SwitchScene : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] string nextScene;
+    [SerializeField] Vector3 NewPosInNextScene;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
             Application.LoadLevel(nextScene);
+            PlayerPrefs.SetFloat("posXStart", NewPosInNextScene.x);
+            PlayerPrefs.SetFloat("posYStart", NewPosInNextScene.y);
+            PlayerPrefs.SetFloat("posZStart", NewPosInNextScene.z);
             Debug.Log(nextScene);
         }
     }
